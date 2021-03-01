1c87c0025b1c477f78d541a4baf9280f3f44c176
codvatechlabs <codvatechlabs@gmail.com>
2021-03-02 01:33:00 +0530

    php-demo
    php-demo

---
+++ php-mysql-crud-demo/README.md
@@ -0,0 +1,5 @@
+AWS RDS and EC2 Demo
+
+Simple Create, Read, Update, Delete (CRUD) using PHP & MySQL
+========
+
---
+++ php-mysql-crud-demo/database.sql
@@ -0,0 +1,13 @@
+/*Create DB Schema as follow*/
+
+create database test;
+
+use test;
+
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL auto_increment,
+  `name` varchar(100) NOT NULL,
+  `age` int(3) NOT NULL,
+  `email` varchar(100) NOT NULL,
+  PRIMARY KEY  (`id`)
+);

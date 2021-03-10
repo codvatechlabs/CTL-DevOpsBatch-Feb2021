fff1c31fcd4614cbd2bb12a6f683d7e293201ebf
codvatechlabs <78929919+codvatechlabs@users.noreply.github.com>
2021-03-10 10:15:46 +0530

    Update provider.tf

--- Terraform/ec2-with-ebs-demo/provider.tf
+++ Terraform/ec2-with-ebs-demo/provider.tf
@@ -1,7 +1,4 @@
 provider "aws" {
   region = "ap-south-1"
-  #shared_credentials_file = "/Users/DELL/.aws/credentials"
-  access_key = "AKIAXKERQJZFLBVYWOZV"
-  secret_key = "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"
-
+  shared_credentials_file = "/Users/DELL/.aws/credentials"
 }
